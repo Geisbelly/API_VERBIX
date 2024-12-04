32b9637cb1c7d03ef94f1492f656e57bec17e1ef
Geisbelly <geisbelly19@gmail.com>
2024-12-04 15:31:04 -0300

    g

--- src/app/api/personagens/[id]/route.ts
+++ src/app/api/personagens/[id]/route.ts
@@ -1,12 +1,12 @@
 import { NextResponse } from 'next/server';
 import { getDbConnection } from '../../../../config/dbConfig';
 
-async function buscarPersonagem(nome: string) {
+async function buscarPersonagem(id: string) {
   try {
     const pool = await getDbConnection();
     const result = await pool
       .request()
-      .input('NOME', nome)
+      .input('NOME', id)
       .query('SELECT * FROM PERSONAGENS WHERE LOWER(NOME) = LOWER(@NOME)');
 
     if (result.recordset.length === 0) {
@@ -31,18 +31,18 @@ async function buscarPersonagem(nome: string) {
   }
 }
 
-export async function GET(req: Request, { params }: { params: { nome: string } }) {
+export async function GET(req: Request, { params }: { params: { id: string } }) {
 
     // O acesso ao `params` precisa ser feito de forma assíncrona
-    const { nome } = await params;
+    const { id } = await params;
 
-  if (!nome) {
+  if (!id) {
     return NextResponse.json(
-      { error: 'Parâmetro "nome" não fornecido' },
+      { error: 'Parâmetro "id" não fornecido' },
       { status: 400 }
     );
   }
 
-  console.log('Nome recebido:', nome);
-  return buscarPersonagem(nome);
+  console.log('id recebido:', id);
+  return buscarPersonagem(id);
 }
